6c9ae1e71d14503ece405b3722edd9e5fa9151e4
tg12 <tg12@users.noreply.github.com>
2020-04-18 23:39:46 +0100

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,14 +1,1 @@
-rm outfile.csv #remove old
-wget http://www.jamessawyer.co.uk/outfile.csv #download sample
-#run the downloader to generate your own
-#cameras, cameras and more cameras
-grep "DVR" outfile.csv > DVR.txt
-#search for index of files
-grep "Index of" outfile.csv > index_of.txt
-#Scary stuff!! Industrial Control systems, France has LOADS!!!
-grep "Schneider" outfile.csv > Schneider.txt
-grep "Industrial" outfile.csv > industrial.txt
-#loads of printers!!!
-grep "HP" outfile.csv > printers.txt
-#Elastic(search)
-grep "search" outfile.csv > search.txt
+***Now use json_server_parser.py***
